2e0627606d04a31653cf8c2fbdb52239c07b4634
Priyank2310 <priyankmaheshwari23@gmail.com>
2023-12-20 16:21:47 +0530

    Add user request validation

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\AddUserRequest;
 use App\Models\Role;
 use App\Models\User;
 use Illuminate\Http\Request;
@@ -33,7 +34,7 @@ class AdminUsersController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(AddUserRequest $request)
     {
         $user = $request->all();
         $user['password'] = Hash::make('Admin123');
