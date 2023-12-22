7dd2f3aca109f3fad7736a0504edc04a7edb7882
Priyank2310 <priyankmaheshwari23@gmail.com>
2023-12-23 00:48:45 +0530

    Add Post model and templates

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\AdminUsersController;
+use App\Http\Controllers\PostController;
 use App\Http\Controllers\CategoryController;
 use Illuminate\Support\Facades\Route;
 
@@ -33,6 +34,8 @@ Route::middleware('auth')->group(function () {
 
 require __DIR__.'/auth.php';
 
+// users
+
 Route::get('/admin', [AdminController::class, 'index'])->middleware('auth')->name('admin');
 
 Route::get('/admin/users', [AdminUsersController::class, 'index'])->middleware('auth')->name('admin-users');
@@ -45,12 +48,27 @@ Route::get('/admin/users/{id}/edit', [AdminUsersController::class, 'edit'])->mid
 
 Route::patch('/admin/users/{id}/edit', [AdminUsersController::class, 'update'])->middleware('admin')->name('admin-users-edit');
 
+// Categories
+
 Route::get('/admin/categories', [CategoryController::class, 'index'])->middleware('auth')->name('admin-categories');
 
 Route::get('/admin/categories/create', [CategoryController::class, 'create'])->middleware('auth')->name('admin-categories-create');
 
 Route::post('/admin/categories/create', [CategoryController::class, 'store'])->middleware('auth')->name('admin-categories-create');
 
-Route::get('/admin/categories/{id}/edit', [CategoryController::class, 'edit'])->middleware('admin')->name('admin-categories-edit');
+Route::get('/admin/categories/{id}/edit', [CategoryController::class, 'edit'])->middleware('auth')->name('admin-categories-edit');
 
-Route::patch('/admin/categories/{id}/edit', [CategoryController::class, 'update'])->middleware('admin')->name('admin-categories-edit');
+Route::patch('/admin/categories/{id}/edit', [CategoryController::class, 'update'])->middleware('auth')->name('admin-categories-edit');
+
+// posts
+
+
+Route::get('/admin/posts', [PostController::class, 'index'])->middleware('auth')->name('admin-posts');
+
+Route::get('/admin/posts/create', [PostController::class, 'create'])->middleware('auth')->name('admin-posts-create');
+
+Route::post('/admin/posts/create', [PostController::class, 'store'])->middleware('auth')->name('admin-posts-create');
+
+Route::get('/admin/posts/{id}/edit', [PostController::class, 'edit'])->middleware('auth')->name('admin-posts-edit');
+
+Route::patch('/admin/posts/{id}/edit', [PostController::class, 'update'])->middleware('auth')->name('admin-posts-edit');
